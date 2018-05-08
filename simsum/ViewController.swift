c4a8c2db795a7f56f394002f79ceb9f79cd434f5
Kirin Patel <kirinpatel@gmail.com>
2018-05-08 19:31:27 -0400

    Switched from Java -> Swift for a mobile app

---
+++ simsum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simsum
+//
+//  Created by Kirin Patel on 5/8/18.
+//  Copyright © 2018 Kirin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
